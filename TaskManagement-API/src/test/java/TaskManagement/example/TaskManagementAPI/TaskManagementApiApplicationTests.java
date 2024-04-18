fa02ab5c1bb693e7d7db05729f9ebd17504ddef7
Winson Baring <winsonbaring10@gmail.com>
2024-04-19 04:48:52 +0800

    initial

--- TaskManagement-API/src/test/java/TaskManagement/example/TaskManagementAPI/TaskManagementApiApplicationTests.java
+++ TaskManagement-API/src/test/java/TaskManagement/example/TaskManagementAPI/TaskManagementApiApplicationTests.java
@@ -1,10 +1,10 @@
-package TaskManagement.example.Api;
+package TaskManagement.example.TaskManagementAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class TaskManagementApiApplicationTests {
 
 	@Test
 	void contextLoads() {
